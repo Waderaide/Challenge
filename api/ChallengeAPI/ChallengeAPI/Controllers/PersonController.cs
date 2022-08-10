ba0bf28b4f0f2c6fcdf611fbb2dd4854fc08113f
Waderaide <103882777@student.swin.edu.au>
2022-08-10 20:44:32 +1000

    m

---
+++ api/ChallengeAPI/ChallengeAPI/Controllers/PersonController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using ChallengeAPI.Models;
+
+namespace ChallengeAPI.Controllers
+{
+    
+        [ApiController]
+        [Route("api/[controller]")]
+        public class PersonController : Controller
+        {
+            private readonly IConfiguration _configuration;
+            private readonly IWebHostEnvironment _env;
+            public PersonController(IConfiguration configuration, IWebHostEnvironment env)
+            {
+                _configuration = configuration;
+                _env = env;
+            }
+
+
+            [HttpGet]
+            [Route("get-all")]
+            public JsonResult Get()
+            {
+                string query = @"
+                            select personId, personName, height from Person
+                            ";
+                DataTable table = new DataTable();
+                string sqlDataSource = _configuration.GetConnectionString("ChallengeDbConnectionString");
+                SqlDataReader myReader;
+                using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+                {
+                    myCon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myCon.Close();
+                    }
+                }
+                return new JsonResult(table);
+            }
+        }
+}
--- api/ChallengeAPI/ChallengeAPI/Models/Person.cs
+++ api/ChallengeAPI/ChallengeAPI/Models/Person.cs
@@ -1,2 +1,13 @@
+﻿
 
 
+namespace ChallengeAPI.Models
+{
+    public class Person
+    {
+        public int personId { get; set; }
+        public string personName { get; set; }
+        public int height { get; set; }
+
+    }
+}
